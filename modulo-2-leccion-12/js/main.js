f55487ae6a6ccc5ba74d6ee80d8fa70d55472793
modehans <monicaglezsanch@gmail.com>
2022-07-11 14:55:47 +0200

    Sort js file lesson 12

--- modulo-2-leccion-12/js/main.js
+++ modulo-2-leccion-12/js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-//Ejercicio 1.1
+//==== Ejercicio 1.1
 const text1=document.querySelector('.js_text1');
 const marks = [5, 4, 6, 7, 9];
 const inflatedMarks = marks.map((value)=>value+1);
@@ -7,7 +7,7 @@ const inflatedMarks = marks.map((value)=>value+1);
 inflatedMarks.forEach((element) => {text1.innerHTML += (`<li>La nota incrementada es ${element}</li>`)});
 
 
-//Ejercicio 1.2
+//==== Ejercicio 1.2
 const text2=document.querySelector('.js_text2');
 const users = [
   { name: "María", isPremium: false },
@@ -17,16 +17,7 @@ const users = [
   { name: "Inmaculada", isPremium: false },
 ];
 
-
-/* 
-const messages=users.map(({name, isPremium})=>{
-  if (isPremium===true){
-    return {saludo: (`Bienvenida ${name}. Gracias por confiar en nosotros.`)};
-
-  }else{
-        return {saludo:(`Bienvenida ${name}.`)};
-  }
-}); */
+// EJERCICIO SEGÚN EXPLICACIONES DE PROGRAMACIÓN HASTA AHORA
 
 const messages=users.map((userData)=>{
   if (userData.isPremium===true){
@@ -37,11 +28,23 @@ const messages=users.map((userData)=>{
   }
 });
 
+console.log(messages);
+
+ messages.forEach((data)=>{text2.innerHTML += `<li> ${data.saludo}</li>`}); //También se puede hacer un for ... of o un for clásico
+
+
+// EJERCICIO CON DESESTRUCTURACIÓN
+
+ /* 
+const messages=users.map(({name, isPremium})=>{
+  if (isPremium===true){
+    return {saludo: (`Bienvenida ${name}. Gracias por confiar en nosotros.`)};
+
+  }else{
+        return {saludo:(`Bienvenida ${name}.`)};
+  }
+});
 
 console.log(messages);
-/* for (const data of messages) {
 
-} */
-messages.forEach((data)=>{text2.innerHTML += `<li> ${data.saludo}</li>`});
-
-/* messages.forEach((${saludo})=>{text2.innerHTML += `<li> ${saludo}</li>`}); */
+messages.forEach(({saludo})=>{text2.innerHTML += `<li> ${saludo}</li>`});  */
